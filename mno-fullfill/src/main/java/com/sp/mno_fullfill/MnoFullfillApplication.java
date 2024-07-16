f6cac3d666c5f09e0ee127fca1b122d6b164c2d4
Dharani <Dharani.javvadi863934@gmail.com>
2024-07-16 08:25:53 +0200

    intial setup

---
+++ mno-fullfill/src/main/java/com/sp/mno_fullfill/MnoFullfillApplication.java
@@ -0,0 +1,13 @@
+package com.sp.mno_fullfill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnoFullfillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnoFullfillApplication.class, args);
+	}
+
+}
---
+++ mno-fullfill/src/test/java/com/sp/mno_fullfill/MnoFullfillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.mno_fullfill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnoFullfillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
